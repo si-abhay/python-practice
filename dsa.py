14135ce726de8a4420ec08c61a373d3dd3881c19
Abhay <asingh20_be20@thapar.edu>
2023-07-20 21:51:35 +0530

    bubble sort

--- dsa.py
+++ dsa.py
@@ -1,29 +1,17 @@
-l=[[1,2,3],[4,5,6],[7,8,9]]
+# Bubble sort
 
-ans=[[] for i in range(len(l))]  #2D Array empty initialization
+# after each iteration one largest element will go to it's final position
+# for eg in l=[10,5,9,4,2]
+# after first iteration 10 will be at it's original pos output of first iteration : [5,9,4,2,10]
+# second iteration [5,4,2,9,10]
+# third iteration [4,2,5,9,10]
 
-for i in range(len(l)):
-    for j in range(len(l[0])):
-        sum=0                    # Look where sum is initialized
-        for x in l[i][j+1:]:     # j+1 is out of bound for last element still it works and thus does nothing to sum and 0 is appended
-            sum+=x
-        ans[i].append(sum)
-
-print(ans)
-
-#=================OPTIMISED n*n approach=========>
-
-l=[[1,2,3],[4,5,6],[7,8,9]]
-
-n = len(l)
-m = len(l[0])
-# Loop to create the new sum which is sum of elements to the right
-for i in range(n):
-    sum = 0
-    for j in range(m-1 , -1, -1):
-        temp = l[i][j]
-        # Storing array value in a temp variable
-        l[i][j] = sum
-        sum += temp
-# Returning the new matrix after performing summation return arr
+l=[10,5,9,4,2]
+n=len(l)
+for i in range(n-1):
+    for j in range(n-1):
+        if l[j]>l[j+1]:
+            temp=l[j]
+            l[j]=l[j+1]
+            l[j+1]=temp
 print(l)
