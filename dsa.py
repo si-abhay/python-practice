2cfb0a7b7d9b330eac38611e30ef8ec79b89792c
Abhay <asingh20_be20@thapar.edu>
2023-07-22 16:26:34 +0530

    linked list

--- dsa.py
+++ dsa.py
@@ -1,7 +1,29 @@
-n=8
-j=2 
+#======= Singly Linked List =======#
 
-mask2=(1<<j)
+class node:
+    def __init__(self,data):        #constructor
+        self.data=data
+        self.next=None
 
-n= (n | mask2)
-print(n)
+class ll:
+    def __init__(self):             #constructor
+        self.head=None
+
+    def print(self):
+        temp=self.head      #basically temp is a temp node type which is first node now
+        while(temp!=None):
+            print(temp.data)
+            temp=temp.next
+
+
+list=ll()                           # create a linked list
+
+first=node(1)
+second=node(2)
+third=node(3)
+
+list.head=first              # head -> first    use list.head not ll.head
+first.next=second
+second.next=third
+
+list.print()                #list.print() not ll.print()
