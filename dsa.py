650b03a00f8df38e4aec438d53ea90e8eb5bebc5
Abhay <asingh20_be20@thapar.edu>
2023-07-23 09:13:33 +0530

    linked list insert

--- dsa.py
+++ dsa.py
@@ -15,10 +15,28 @@ class ll:
             print(temp.data)
             temp=temp.next
     
-    def insert(self,data):                  # insert node at beginning
+    def insert_begin(self,data):                  # insert node at beginning
         new=node(data)
         new.next=self.head
         self.head=new
+
+    def insert_end(self,data):                     # insertion at end
+        new=node(data)
+        if self.head is None:
+            self.head=new
+            return
+        temp=self.head
+        while(temp.next!=None):
+            temp=temp.next
+        temp.next=new
+
+    def insert_mid(self,mid,data):              # insert AFTER given node if present
+        if mid is None:
+            print("No such node")
+            return
+        new=node(data)
+        new.next=mid.next
+        mid.next=new
 
 
 
@@ -31,6 +49,7 @@ third=node(3)
 list.head=first              
 first.next=second
 second.next=third
-
-list.insert(0)
+list.insert_begin(0)
+list.insert_end(4)
+list.insert_mid(third,3.5)
 list.print()           
