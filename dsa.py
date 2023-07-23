ff5aefe2672bcd6e8f89deaed3b2bf0b9d8ae121
Abhay <asingh20_be20@thapar.edu>
2023-07-23 08:37:00 +0530

    linked list insertions

--- dsa.py
+++ dsa.py
@@ -1,29 +1,36 @@
 #======= Singly Linked List =======#
 
 class node:
-    def __init__(self,data):        #constructor
+    def __init__(self,data):
         self.data=data
         self.next=None
 
 class ll:
-    def __init__(self):             #constructor
+    def __init__(self):        
         self.head=None
 
     def print(self):
-        temp=self.head      #basically temp is a temp node type which is first node now
+        temp=self.head   
         while(temp!=None):
             print(temp.data)
             temp=temp.next
+    
+    def insert(self,data):                  # insert node at beginning
+        new=node(data)
+        new.next=self.head
+        self.head=new
 
 
-list=ll()                           # create a linked list
+
+list=ll()        
 
 first=node(1)
 second=node(2)
 third=node(3)
 
-list.head=first              # head -> first    use list.head not ll.head
+list.head=first              
 first.next=second
 second.next=third
 
-list.print()                #list.print() not ll.print()
+list.insert(0)
+list.print()           
