4a166da7cd02bd8cb4eb47d0161f64dd8a94bc09
Abhay <asingh20_be20@thapar.edu>
2023-07-25 13:23:24 +0530

    cdjcdj

--- dsa.py
+++ dsa.py
@@ -1,18 +1,1 @@
-l=[-5, 4, 6, -3, 4, 1]
-max_sum=-float('inf')
-n=len(l)
-sum=0
-ans=[]
-ls=[]
-for i in range(n):
-    sum+=l[i]
-    ls.append(l[i])
-    if sum<0 and i !=0:
-        sum=0
-        ls.clear()
-    if max_sum<sum:
-        max_sum=sum
-        ans=ls[0:]
-
-print(max_sum)
-print(ans)
+testing ocdee
