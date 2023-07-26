7ddbf668ef33ad96720f3e23afd4af6d9e34d469
Abhay <asingh20_be20@thapar.edu>
2023-07-26 11:01:08 +0530

    RECURSIVE LL

--- dsa.py
+++ dsa.py
@@ -1,1 +1,31 @@
-can commit message changed
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+def create(arr):
+    head=None
+    last=None
+
+    for i in arr:
+        new=node(i)
+
+        if head is None:
+            head=new
+            last=head
+        else:
+            last.next=new
+            last=new
+    
+    return head
+
+def printf(head):
+    if head is None:
+        return
+    print(head.data)
+    return printf(head.next)
+
+if __name__ == "__main__":
+    arr=[1,2,3,4,5]
+    start=create(arr)
+    printf(start)
