49ef1108f6daa9049de6fed51334f54d185e98b4
Abhay <asingh20_be20@thapar.edu>
2023-07-29 23:02:35 +0530

    dsjgcdj

--- dsa.py
+++ dsa.py
@@ -1,19 +1,7 @@
-'''
-class Solution(object):     #22/28 test case passed
+class Solution(object): 
     def queryString(self, s, n):
-        q=[]
-        q.append("1")
-        for i in range(1,n):
-            temp=q.pop(0)
-            if (temp+"0" in s) and (temp+"1" in s):
-                q.append(temp+"0")
-                q.append(temp+"1")
-            else:
-                return False  
+        for i in range(1, n+1):
+            binary_rep = bin(i)[2:]
+            if binary_rep not in s:
+                return False
         return True
-'''
-
-
-s="1011"
-i=int(s)
-print(i)
