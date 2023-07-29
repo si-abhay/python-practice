308fd2bcbd9fa014293684a9e7820121491718df
Abhay <asingh20_be20@thapar.edu>
2023-07-29 23:11:09 +0530

    dsjgcdjsd

--- dsa.py
+++
@@ -1,7 +0,0 @@
-class Solution(object): 
-    def queryString(self, s, n):
-        for i in range(1, n+1):
-            binary_rep = bin(i)[2:]
-            if binary_rep not in s:
-                return False
-        return True
