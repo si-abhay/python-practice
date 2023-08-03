0c06eb935569b763a9d1f22207f6f727c9016542
Abhay <asingh20_be20@thapar.edu>
2023-08-03 16:31:00 +0530

    xcdcdf

--- dsa.py
+++ dsa.py
@@ -1,15 +1,11 @@
 class Animal:
-    def __init__(self, age, name):
-        self.age = age
-        self.name = name
-        print("Animal class created")
+    def does(self):
+        print("speaks")
 
-    def copy(self):         # Copy constructor functionality C++
-        new_animal = type(self)(self.age, self.name)
-        return new_animal
+class Dog(Animal):
+    pass
+    #def does(self):
+    #    print("barks")
 
-ani = Animal(20, "RAT")
-mni = ani.copy()
-
-print(mni.name)
-print(mni.age)
+d=Dog()
+d.does()
