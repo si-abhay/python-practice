6c2c14e33105162ae6138ef47c377d0c64d84c0d
Abhay <asingh20_be20@thapar.edu>
2023-08-04 16:14:56 +0530

    xcdddedd

--- dsa.py
+++ dsa.py
@@ -1,20 +1,27 @@
-# Multiple Inheritance
+'''# Multiple Inheritance
 class Student:
     def __init__(self,date):
         self.date=date
-        print("Student constructor")
+        print(f"Student 1st constructor {}")
+
+    def __init__(self):
+        print("Student 2nd constructor")
     def study(self):
         print("Studying")
 
-class Teacher:
-    def __init__(self,name):
-        self.name=name
-        print("Teacher constructor")
-    def teach(self):
-        print("Teacher")
+s=Student(2)
+'''
 
-class TA (Student, Teacher):
-    def work(self):
-        print("Working")
 
-t = TA()
+class Solution(object):     # 36/46 test case passsed
+    def wordBreak(self, s, wordDict):
+        for x in wordDict:
+            ans=""
+            m=s.split(x)
+            for y in m:
+                ans+=y
+            s=ans
+        if len(s)==0:
+            return True
+        else:
+            return False
