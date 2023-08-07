375c7ee6ce91718753c56beaf397a3a3be9f28ee
Abhay <asingh20_be20@thapar.edu>
2023-08-07 15:08:17 +0530

    xcdxds

--- dsa.py
+++ dsa.py
@@ -1,2 +1,14 @@
-# class and objects sizes
-# ==== TO DO ========
+class animal:
+    def __init__(self):
+        print("Animal called")
+
+class snake:
+    def __init__(self):
+        print("Snake called")
+    
+class snail(animal,snake):
+    def __init__(self):
+        super().__init__()
+        print("snail called")
+
+obj=snail()  
