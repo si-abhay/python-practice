31a085e798b2d40a8993bc5c1ddcba0d9d5876e5
Abhay <asingh20_be20@thapar.edu>
2023-08-14 20:20:23 +0530

    fvdsfvdfbcfvfdc

--- dsa.py
+++ dsa.py
@@ -1,17 +1,31 @@
-def heap_max(list,value):
-    n=len(list)
-    list.append(value)
-    i=n                  # since indexing is from 1 , 0th element is considered as garbage
-    while(i>1):
-        parent = i//2             # // is floor division
-        if list[parent] < list[i]:
-            list[i],list[parent]=list[parent],list[i]
-            i=parent
-        else:
-            return
+def heap_max_del(l):
+    n=len(l)
+    i=0
+    l[0]=l.pop()
+    n-=1
 
-#l=["m",50,30,40,10,5,20,30]
-#heap_max(l,60)
-l=['m', 60, 50, 40, 30, 5, 20, 30, 10]
-heap_max(l,45)
+    while i < n:
+        left_idx = 2 * i + 1
+        right_idx = 2 * i + 2
+
+        if left_idx < n:
+            left = l[left_idx]
+        else:
+            left = float('-inf')  # Negative infinity if left child doesn't exist
+
+        if right_idx < n:
+            right = l[right_idx]
+        else:
+            right = float('-inf')  # Negative infinity if right child doesn't exist
+
+        larger_idx = left_idx if left > right else right_idx
+
+        if l[i] < l[larger_idx]:
+            l[i], l[larger_idx] = l[larger_idx], l[i]
+            i = larger_idx
+        else:
+            break
+
+l=[40,30,10,20,15]
+heap_max_del(l)
 print(l)
