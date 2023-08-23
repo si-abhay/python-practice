06e56a71c0a88678a5eb9f84ba9648ce3cb5fc80
Abhay <asingh20_be20@thapar.edu>
2023-08-24 01:55:06 +0530

    cddcd

--- dsa.py
+++ dsa.py
@@ -7,10 +7,3 @@ class Solution:
         freq = [[] for i in range(len(nums)+1)]
         for n,count in maps.items():
             freq[count].append(n)
-
-        res = []
-        for i in range(len(freq) - 1, -1, -1):
-            for x in freq[i]:
-                res.append(x)
-            if len(res) == k:
-                return res
