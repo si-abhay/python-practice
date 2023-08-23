a175c67ebd54339632a464fb730a34d4faccab8e
Abhay <asingh20_be20@thapar.edu>
2023-08-24 01:55:18 +0530

    cddcd

--- dsa.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def topKFrequent(self, nums, k):
-        maps={}
-        for n in nums:
-            maps[n]= 1 + maps.get(n,0)
-
-        freq = [[] for i in range(len(nums)+1)]
-        for n,count in maps.items():
-            freq[count].append(n)
