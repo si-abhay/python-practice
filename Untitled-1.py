3b2236a00a1cbd8cca0c13d77172ae953335a1dc
Abhay <asingh20_be20@thapar.edu>
2023-09-12 19:13:26 +0530

    djnddk

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+# //20,3,1,19,21,4,2
+# (1,2,3,4)(19,20,21)
+global maxi
+def _lis(arr,n):
+    
+    if n==1:
+        return 1
+    maxEndhere=1
+    for i in range(1,n):
+        res=_lis(arr,i)
+        if arr[i-1]<arr[n-1] and res+1>maxEndhere:
+            maxEndhere=res+1
+    maxi=max(maxi,maxEndhere)
+    return maxEndhere
+def lis(arr):
+    n=len(arr)
+    maxi=1
+    _lis(arr,n)
+    print(maxi)
+arr=[20,3,1,19,21,4,2]
+lis(arr)
